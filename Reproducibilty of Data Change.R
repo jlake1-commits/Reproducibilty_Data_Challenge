1260d02fa9abddd44c2c0af3a8415eb05eeeb9b6
Tegan <tlmarianchuk@gmail.com>
2019-09-13 11:15:31 -0400

    Cleaned up final figures
    added more clarifying plot titles and legend. coordinated Shuo's variables for code consistency

--- Reproducibilty of Data Change.R
+++ Reproducibilty of Data Change.R
@@ -74,44 +74,45 @@ sowbugs_plot
 
 
 #PLotting same graph but with dLGP data
-sowbug_plot <- ggplot(cdat, aes(x=cdat$k_number_of_arthropods, y=p_normsowbug)) +
+sowbug_plot <- ggplot(cdat, aes(x=cdat$k_number_of_arthropods, y=p_normsowbugs)) +
   geom_point(size = 3) + xlab("") + ylab("") +
-  geom_line(data=cdat, aes(x=cdat$k_number_of_arthropods, y=p_dLGP_sowbug), linetype="dotted", colour='red') +  
-  geom_point(data=cdat, aes(x=cdat$k_number_of_arthropods, y=p_dLGP_sowbug),colour='red', shape=0, size = 3)
+  geom_line(data=cdat, aes(x=cdat$k_number_of_arthropods, y=p_dLGP_sowbugs), linetype="dotted", colour='red') +  
+  geom_point(data=cdat, aes(x=cdat$k_number_of_arthropods, y=p_dLGP_sowbugs),colour='red', shape=0, size = 3)
 sowbug_plot
 
 library(ggplot2)
 library(RMKdiscrete)
 library(cowplot)
-spider_mean <- sum(dat$C_count_of_boards_with_k_spiders * dat$k_number_of_arthropods)/sum(dat$C_count_of_boards_with_k_spiders)
-sowbug_mean <- sum(dat$C_count_of_boards_with_k_sowbugs * dat$k_number_of_arthropods)/sum(dat$C_count_of_boards_with_k_sowbugs)
-egg_mean <- sum(dat2$C_count_of_beans_with_k_eggs * dat2$k_number_of_eggs)/sum(dat2$C_count_of_beans_with_k_eggs)
+spider_mean <- sum(cdat$C_count_of_boards_with_k_spiders * cdat$k_number_of_arthropods)/sum(cdat$C_count_of_boards_with_k_spiders)
+sowbug_mean <- sum(cdat$C_count_of_boards_with_k_sowbugs * cdat$k_number_of_arthropods)/sum(cdat$C_count_of_boards_with_k_sowbugs)
+egg_mean <- sum(mdat$C_count_of_beans_with_k_eggs * mdat$k_number_of_eggs)/sum(mdat$C_count_of_beans_with_k_eggs)
 
-spider_p <- dpois(dat$k_number_of_arthropods,spider_mean)
-sowbug_p <- dpois(dat$k_number_of_arthropods,sowbug_mean)
-egg_p <- dpois(dat2$k_number_of_eggs, egg_mean)
+spider_p <- dpois(cdat$k_number_of_arthropods,spider_mean)
+sowbug_p <- dpois(cdat$k_number_of_arthropods,sowbug_mean)
+egg_p <- dpois(mdat$k_number_of_eggs, egg_mean)
 
-dat$C_count_of_boards_with_k_spiders <- as.numeric(dat$C_count_of_boards_with_k_spiders)
-dat$C_count_of_boards_with_k_sowbugs <- as.numeric(dat$C_count_of_boards_with_k_sowbugs)
-dat2$k_number_of_eggs <- as.numeric(dat2$k_number_of_eggs)
+dat$C_count_of_boards_with_k_spiders <- as.numeric(cdat$C_count_of_boards_with_k_spiders)
+dat$C_count_of_boards_with_k_sowbugs <- as.numeric(cdat$C_count_of_boards_with_k_sowbugs)
+dat2$k_number_of_eggs <- as.numeric(mdat$k_number_of_eggs)
 
-plot_spider <- ggplot(data = dat + spider_p, aes(x = dat$k_number_of_arthropods, y = spider_p)) + geom_smooth() + geom_point() + labs(x = "Spider Counts", y = "Probability")
-plot_sowbug <- ggplot(data = dat + sowbug_p, aes(x = dat$k_number_of_arthropods, y = sowbug_p)) + geom_smooth() + geom_point() + labs(x = "Sowbug Counts", y = "Probability")
-plot_egg <- ggplot(data = dat2 + egg_p, aes(x = dat2$k_number_of_eggs, y = egg_p)) + geom_smooth() + geom_point() + labs(x = "Egg Counts", y = "Probability")
+plot_spider <- ggplot(data = cdat + spider_p, aes(x = cdat$k_number_of_arthropods, y = spider_p)) + geom_smooth() + geom_point() + labs(x = "Spider Counts", y = "Probability")
+plot_sowbug <- ggplot(data = cdat + sowbug_p, aes(x = cdat$k_number_of_arthropods, y = sowbug_p)) + geom_smooth() + geom_point() + labs(x = "Sowbug Counts", y = "Probability")
+plot_egg <- ggplot(data = mdat + egg_p, aes(x = mdat$k_number_of_eggs, y = egg_p)) + geom_smooth() + geom_point() + labs(x = "Egg Counts", y = "Probability")
 
-spider_LGP <- dLGP(dat$k_number_of_arthropods, theta = spider_mean, lambda = 0)
-sowbug_LGP <- dLGP(dat$k_number_of_arthropods, theta = (sowbug_mean*(1-0.53214)), lambda = 0.53214)
-egg_LGP <- dLGP (dat2$k_number_of_eggs, theta = (egg_mean*(1-lambda2)), lambda = lambda2)
+spider_LGP <- dLGP(cdat$k_number_of_arthropods, theta = spider_mean, lambda = 0)
+sowbug_LGP <- dLGP(cdat$k_number_of_arthropods, theta = (sowbug_mean*(1-0.53214)), lambda = 0.53214)
+egg_LGP <- dLGP (mdat$k_number_of_eggs, theta = (egg_mean*(1-lambda2)), lambda = lambda2)
 
-plot_spider_origin <-ggplot(data = dat, aes(x = dat$k_number_of_arthropods, y = dat$C_count_of_boards_with_k_spiders)) + geom_smooth() + geom_point() + labs(x = "Spider Counts", y = "Number of Boards")
-plot_sowbug_origin <-ggplot(data = dat, aes(x = dat$k_number_of_arthropods, y = dat$C_count_of_boards_with_k_sowbugs)) + geom_smooth() + geom_point() + labs(x = "Sowbug Counts", y = "Number of Boards")
-plot_egg_origin <-ggplot(data = dat2, aes(x = dat2$k_number_of_eggs, y = dat2$C_count_of_beans_with_k_eggs)) + geom_smooth() + geom_point() + labs(x = "Egg Counts", y = "Number of Spots")
+plot_spider_origin <-ggplot(data = cdat, aes(x = cdat$k_number_of_arthropods, y = cdat$C_count_of_boards_with_k_spiders)) + geom_smooth() + geom_point() + labs(x = "Spider Counts", y = "Number of Boards")
+plot_sowbug_origin <-ggplot(data = cdat, aes(x = cdat$k_number_of_arthropods, y = cdat$C_count_of_boards_with_k_sowbugs)) + geom_smooth() + geom_point() + labs(x = "Sowbug Counts", y = "Number of Boards")
+plot_egg_origin <-ggplot(data = mdat, aes(x = mdat$k_number_of_eggs, y = mdat$C_count_of_beans_with_k_eggs)) + geom_smooth() + geom_point() + labs(x = "Egg Counts", y = "Number of Spots")
 
 plotSpider <- plot_grid(plot_spider,plot_spider_origin)
 plotSowbug <- plot_grid(plot_sowbug,plot_sowbug_origin)
 plotEgg <- plot_grid(plot_egg,plot_egg_origin)
 
 lambda2 = 0.92314
-plot_sowbug_LGP <- ggplot(data = dat + sowbug_p + sowbug_LGP) + geom_point(aes(x = dat$k_number_of_arthropods, y = sowbug_p)) + geom_point(aes(x = dat$k_number_of_arthropods, y = sowbug_LGP)) +labs(x = "Sowbug Counts", y = "Probability") + geom_smooth(aes(x = dat$k_number_of_arthropods, y = sowbug_p)) + geom_smooth(aes(x = dat$k_number_of_arthropods, y = sowbug_LGP,color = "LGP"))
-plot_egg_LGP <- ggplot(data = dat2 + egg_p + egg_LGP) + geom_point(aes(x = dat2$k_number_of_eggs, y = egg_p)) + geom_point(aes(x = dat2$k_number_of_eggs, y = egg_LGP)) +labs(x = "Egg Counts", y = "Probability") + geom_smooth(aes(x = dat2$k_number_of_eggs, y = egg_p)) + geom_smooth(aes(x = dat2$k_number_of_eggs, y = egg_LGP, color = "LGP"))
-plot_spider_LGP <- ggplot(data = dat + spider_p + spider_LGP) + geom_point(aes(x = dat$k_number_of_arthropods, y = spider_p)) + geom_point(aes(x = dat$k_number_of_arthropods, y = spider_LGP)) +labs(x = "Sowbug Counts", y = "Probability") + geom_smooth(aes(x = dat$k_number_of_arthropods, y = spider_p)) + geom_smooth(aes(x = dat$k_number_of_arthropods, y = spider_LGP, color ="LGP"))
+plot_sowbug_LGP <- ggplot(data = cdat + sowbug_p + sowbug_LGP) + geom_point(aes(x = cdat$k_number_of_arthropods, y = sowbug_p)) + geom_point(aes(x = cdat$k_number_of_arthropods, y = sowbug_LGP)) +labs(title="Probability Distributions for Sowbug Counts", x = "Sowbug Counts", y = "Probability") + theme(plot.title = element_text(hjust = 0.5)) + geom_smooth(aes(x = cdat$k_number_of_arthropods, y = sowbug_p, color="Poisson")) + geom_smooth(aes(x = cdat$k_number_of_arthropods, y = sowbug_LGP,color = "LGP"))
+plot_egg_LGP <- ggplot(data = mdat + egg_p + egg_LGP) + geom_point(aes(x = mdat$k_number_of_eggs, y = egg_p)) + geom_point(aes(x = mdat$k_number_of_eggs, y = egg_LGP)) +labs(title="Probability Distributions for Egg Counts", x = "Egg Counts", y = "Probability") + theme(plot.title = element_text(hjust = 0.5)) + geom_smooth(aes(x = mdat$k_number_of_eggs, y = egg_p, color="Poisson")) + geom_smooth(aes(x = mdat$k_number_of_eggs, y = egg_LGP, color = "LGP"))
+plot_spider_LGP <- ggplot(data = cdat + spider_p + spider_LGP) + geom_point(aes(x = cdat$k_number_of_arthropods, y = spider_p)) + geom_point(aes(x = cdat$k_number_of_arthropods, y = spider_LGP)) +labs(title="Probability Distributions for Spider Counts", x = "Sowbug Counts", y = "Probability") + theme(plot.title = element_text(hjust = 0.5)) + geom_smooth(aes(x = cdat$k_number_of_arthropods, y = spider_p, color="Poisson")) + geom_smooth(aes(x = cdat$k_number_of_arthropods, y = spider_LGP, color ="LGP"))
+
